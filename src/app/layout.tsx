b67e886c1216f6520de6dfdf9ad84ccda2e264af
Carlos Checa <carlos@checa.dev>
2025-01-24 22:47:45 +0100

    fix: default background colour

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -7,7 +7,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body className="bg-grey-50">{children}</body>
     </html>
   );
 }
