1821afc508d64616e4fadd810d8287fc06727368
Carlos Checa <carlos@checa.dev>
2025-02-28 22:04:30 +0100

    fix: duplicated css classes

--- src/app/(auth)/login/ui/loginForm.tsx
+++ src/app/(auth)/login/ui/loginForm.tsx
@@ -159,7 +159,7 @@ export default function LoginForm() {
                 type="submit"
                 disabled={button}
                 className={
-                  "disabled:bg-primary-700 flex w-full justify-center rounded-md bg-primary-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-xs hover:bg-primary-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-primary-600"
+                  "disabled:bg-primary-700 flex w-full justify-center rounded-md bg-primary-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-xs hover:bg-primary-500 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-primary-600"
                 }
               >
                 Sign in
