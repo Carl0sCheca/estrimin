a09eda4c1fe89b16ef8cfe1e500fa27976fe8eb3
Carlos Checa <carlos@checa.dev>
2026-02-12 00:47:33 +0000

    feat: add symlink creation for recording path based on STREAM_URL

--- src/scripts/startup.ts
+++ src/scripts/startup.ts
@@ -2,8 +2,26 @@ import prisma from "@/lib/prisma";
 import { SITE_SETTING } from "@/interfaces";
 import { registerAction } from "@/actions";
 import { Role } from "@/generated/client";
+import { getAllUrlSegment, hasPathname } from "@/lib/utils-server";
+import { lstat, symlink, unlink } from "fs/promises";
+import { join } from "path";
 
 const main = async () => {
+  if (await hasPathname(process.env.STREAM_URL || "")) {
+    const recordingPath = process.env.RECORDINGS_PATH || "";
+    const urlSegments =
+      (await getAllUrlSegment(process.env.STREAM_URL || "")) || [];
+
+    const linkPath = join(recordingPath, "recordings");
+    const target = join(recordingPath, urlSegments.join("/"));
+
+    try {
+      await lstat(linkPath);
+      await unlink(linkPath);
+    } catch {}
+    await symlink(target, linkPath, "dir");
+  }
+
   const forbiddenNames = [
     "user",
     "admin",
