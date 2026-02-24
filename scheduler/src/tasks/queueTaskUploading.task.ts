7d13657dcf17e87ed38673d41f927a546c87ffdc
Carlos Checa <carlos@checa.dev>
2026-02-24 00:10:59 +0000

    feat: [wip] implement queue task for uploading recordings
    Signed-off-by: Carlos Checa <carlos@checa.dev>

--- scheduler/src/tasks/queueTaskUploading.task.ts
+++ scheduler/src/tasks/queueTaskUploading.task.ts
@@ -1,3 +1,5 @@
+import { RecordingQueue } from "@/generated/client";
+import { RecordingQueueState } from "@/generated/enums";
 import { SITE_SETTING } from "@/interfaces";
 import prisma from "@/lib/prisma";
 import { JOB_UPLOADING_QUEUE } from "@scheduler/jobs";
@@ -16,6 +18,50 @@ export const queueTaskUploading = async () => {
   }
 
   console.info(`Running queueTaskUploading at ${new Date()}`);
+
+  while (true) {
+    const recording = await prisma.$transaction(async (tx) => {
+      const record = await tx.$queryRaw<Array<RecordingQueue>>`
+      SELECT * FROM "recordingQueue"
+      WHERE "status" = ${RecordingQueueState.RECORDED}
+      LIMIT 1
+      FOR UPDATE SKIP LOCKED
+    `;
+
+      if (record.length === 0) {
+        return null;
+      }
+
+      return await tx.recordingQueue.update({
+        where: { id: record[0].id },
+        data: {
+          status: RecordingQueueState.UPLOADING,
+          startedAt: new Date(),
+        },
+      });
+    });
+
+    if (!recording) {
+      break;
+    }
+
+    const isUsingS3Bucket = recording.fileName.startsWith("s3://");
+
+    if (!isUsingS3Bucket) {
+      await prisma.recordingQueue.update({
+        where: {
+          id: recording.id,
+        },
+        data: {
+          status: RecordingQueueState.UPLOADED,
+          finishedAt: new Date(),
+        },
+      });
+
+      continue;
+    } else {
+    }
+  }
 
   await updateLastExecutionFromSettings(JOB_UPLOADING_QUEUE);
 };
