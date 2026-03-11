743da53074c17f4f01f9a52bc06ba43bd986f73f
Carlos Checa <carlos@checa.dev>
2026-03-11 21:08:11 +0100

    refactor: rename task file
    Signed-off-by: Carlos Checa <carlos@checa.dev>

--- scheduler/src/jobs/retry-failed-queue.job.ts
+++ scheduler/src/jobs/retry-failed-queue.job.ts
@@ -1,5 +1,5 @@
 import { AsyncTask, SimpleIntervalJob } from "toad-scheduler";
-import { queueTaskRetryFailedRecordings } from "@scheduler/tasks/queue-retry-failed-recordings.tasks";
+import { queueTaskRetryFailedRecordings } from "@scheduler/tasks/queue-retry-failed-recordings.task";
 
 export const JOB_RETRY_FAILED_QUEUE = "JOB_RETRY_FAILED_QUEUE";
 
